53355ee2245a37878239e81b3185d9fa660b34a5
loubruness <94389521+loubruness@users.noreply.github.com>
2025-01-03 22:06:11 +0100

    connexion fonctionnent, suppression des dossiers inutiles

--- magic-cloud-auth/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.magic_cloud_authentication;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    // User findByEmail(String email); // Custom query to find a user by email
-}
--- magic-cloud-auth/src/main/java/com/example/backend/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend;
-
-// import com.example.backend.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
